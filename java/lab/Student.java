8b06637de58ed64b8fda56d34772bd3d2e3a1ae0
Mourya-804 <150243238+Mourya-804@users.noreply.github.com>
2023-12-13 13:40:06 +0530

    Add files via upload

---
+++ java/lab/Student.java
@@ -0,0 +1,14 @@
+package Static.Related.classes;
+
+public class Student {
+  private String studentName;
+  private String collegeName;
+  private int studentId;
+  private String m= "Successful!" ;
+  public static void main(String args[])
+  {
+	  Student s=new Student();
+	  
+	  System.out.println(s.m);
+  }
+}
