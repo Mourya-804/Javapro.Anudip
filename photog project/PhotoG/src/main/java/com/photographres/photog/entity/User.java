c204fd567a22e39d7af31356e5b0e9bdb9815b54
Mourya-804 <150243238+Mourya-804@users.noreply.github.com>
2024-01-28 15:20:24 +0530

    Update User.java

--- photog project/PhotoG/src/main/java/com/photographres/photog/entity/User.java
+++ photog project/PhotoG/src/main/java/com/photographres/photog/entity/User.java
@@ -1,4 +1,11 @@
+
+	
 package com.photographres.photog.entity;
+
+import java.util.List;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 
@@ -7,6 +14,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -48,7 +56,12 @@ public class User {
 	private long uPhone;
 	
 	@Column(length=40)
+	
 	private String addr;
+
+  @OneToMany(mappedBy = "user")
+	@JsonManagedReference
+	List<Bookings> bookingList;
 
    
       
